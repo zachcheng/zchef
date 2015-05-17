15f24326cb868bd27c4cb747cca6b3b636055e48
zhi <zhi@Zaizhis-MacBook-Air-2.local>
2015-05-17 20:24:55 +0800

    put in ssh_wrapper for repo

--- goa_dev/attributes/default.rb
+++ goa_dev/attributes/default.rb
@@ -1,4 +1,4 @@
 hostname = node[:opsworks][:instance][:hostname]
 default[:source][:user] = node[:developers][hostname] || 'zdefault'
-default[:source][:path] = "/home/src/goa"
+default[:source][:path] = "/home/src"
 default[:source][:repository_host] = "git@bitbucket.org:zachcheng/goa.git"
--- goa_dev/recipes/load_repo.rb
+++ goa_dev/recipes/load_repo.rb
@@ -1,6 +1,15 @@
 source_path = node[:source][:path]
 source_user = node[:source][:user]
 repository_host  = node[:source][:repository_host]
+repository_ssh_private_key = node[:deploy]['gemsapp'][:scm][:ssh_key]
+repository_ssh_private_key_path = '/tmp/id_rsa_gems'
+ssh_wrapper_path = '/tmp/ssh_wrapper.sh'
+
+template ssh_wrapper_path do
+  source 'chef_deploy_ssh_wrapper.erb'
+  variables :ssh_key_path => repository_ssh_private_key_path
+  action :create
+end
 
 directory source_path do
   owner source_user
@@ -12,10 +21,16 @@ git source_path do
   action :sync
   repository repository_host
   enable_submodules true
+  ssh_wrapper ssh_wrapper_path
 end 
 
 execute "checkout master branch on repo" do
   cwd "#{source_path}"
   command "git checkout master"
 end 
+
+file repository_ssh_private_key_path do
+  action :delete
+end
+
 
