b018b908d062bd0710ece57f7ac6cd77e2fc3029
zhi <zhi@Zaizhis-MacBook-Air-2.local>
2015-05-23 17:01:14 +0800

    added user to rvm commands

--- dev_setup/recipes/default.rb
+++ dev_setup/recipes/default.rb
@@ -2,9 +2,11 @@ developer = node[:developers][node[:opsworks][:instance][:hostname]] || 'zdefaul
 
 execute "get keys for RVM installation" do
   command "gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3"
+  user developer
 end 
 
 execute "install RVM for single user" do
   command "curl -sSL https://get.rvm.io | bash -s stable"
+  user developer
 end 
 
