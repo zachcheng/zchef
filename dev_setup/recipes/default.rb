4ff3e8274c9c82b167d817cd28c34e417f891483
zhi <zhi@Zaizhis-MacBook-Air-2.local>
2015-05-23 17:26:32 +0800

    set up env for gpg to put the files into the correct place

--- dev_setup/recipes/default.rb
+++ dev_setup/recipes/default.rb
@@ -2,8 +2,9 @@ developer = node[:developers][node[:opsworks][:instance][:hostname]] || 'zdefaul
 
 execute "get keys for RVM installation" do
   cwd "/home/#{developer}"
-  command "gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3"
+  enviroment ({ 'GNUPGHOME' => "/home/#{developer}"})
   user "#{developer}"
+  command "gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3"
 end 
 
 execute "install RVM for single user" do
