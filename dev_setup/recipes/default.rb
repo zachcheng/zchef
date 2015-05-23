1c052f90e5895c274f1465da8153b3200ccd763b
zhi <zhi@Zaizhis-MacBook-Air-2.local>
2015-05-23 17:08:02 +0800

    added cwd for rvm commands

--- dev_setup/recipes/default.rb
+++ dev_setup/recipes/default.rb
@@ -1,11 +1,13 @@
 developer = node[:developers][node[:opsworks][:instance][:hostname]] || 'zdefault'
 
 execute "get keys for RVM installation" do
+  cwd "/home/#{developer}"
   command "gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3"
   user developer
 end 
 
 execute "install RVM for single user" do
+  cwd "/home/#{developer}"
   command "curl -sSL https://get.rvm.io | bash -s stable"
   user developer
 end 
