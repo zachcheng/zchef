b503343a64374a0f71e788b4736dded4a68ad4b9
zhi <zhi@Zaizhis-MacBook-Air-2.local>
2015-05-24 11:07:22 +0800

    bugfix

--- goa_dev/attributes/default.rb
+++ goa_dev/attributes/default.rb
@@ -1,5 +1,5 @@
 hostname = node[:opsworks][:instance][:hostname]
 default[:source][:user] = node[:developers][hostname] || 'zdefault'
-default[:source][:path] = "/home/src/goa"
+default[:source][:path] = "/home/src"
 default[:source][:container_path] = "/home/src/goa"
 default[:source][:repository_host] = "git@bitbucket.org:zachcheng/goa.git"
